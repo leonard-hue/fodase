27bbf0649e06ea84e7280a0cf6818d4d549b4d67
leonard-hue <Leogoncalvesdossantos007@gmail.com>
2024-11-28 21:24:55 -0300

    Add files via upload

---
+++ flashcard/perguntas.js
@@ -0,0 +1,20 @@
+criaCartao(
+    'Programação',
+    'oque é javascrpt',
+    'é uma linguagem de programação'
+)
+criaCartao(
+    'geografia',
+    'qual a capital da frança',
+    'a capital da frança é paris'
+)
+criaCartao(
+    'Programação',
+    'oque é uma funçao',
+    'é um bloco de codigos que executa alguma tarefa'
+)
+criaCartao(
+    'lingua inglesa',
+    'como se diz oi em ingles',
+    'oi em ingles se diz hi '
+)
